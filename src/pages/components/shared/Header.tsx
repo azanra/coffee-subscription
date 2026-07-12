783a896a9a380fca97a4c1e97cc4c34f473d80c5
azan-personal <112457939+azanra@users.noreply.github.com>
2026-07-12 08:47:51 +0700

    feat: adjust the header layout

--- src/pages/components/shared/Header.tsx
+++ src/pages/components/shared/Header.tsx
@@ -1,26 +1,34 @@
 import { Link } from "react-router";
-import logo from "../../../assets/shared/desktop/logo.svg";
+import CoffeeLogo from "../../../assets/shared/desktop/logo.svg";
+import HamburgerLogo from "../../../assets/shared/mobile/icon-hamburger.svg";
+import useIsMobileScreen from "../../../shared/hooks/useIsMobileScreen";
 
 const Header = () => {
-  const navigation = [
+  const { isMobileScreen } = useIsMobileScreen();
+
+  const navigationTab = [
     { to: "/", text: "home" },
     { to: "/about", text: "about us" },
     { to: "/plan", text: "create your plan" },
   ];
+
   return (
-    <div>
-      <div>
-        <img src={logo} alt="coffee" />
-      </div>
-      <div>
-        {navigation.map((nav) => {
-          return (
-            <Link replace to={nav.to} key={nav.to}>
-              {nav.text.toUpperCase()}
-            </Link>
-          );
-        })}
-      </div>
+    <div className="flex justify-between mb-[40px]">
+      <img src={CoffeeLogo} alt="coffee-logo" />
+
+      {isMobileScreen ? (
+        <img src={HamburgerLogo} alt="hamburger-logo" />
+      ) : (
+        <div className="flex gap-[33px]">
+          {navigationTab.map((nav) => {
+            return (
+              <Link to={nav.to} key={nav.to}>
+                {nav.text.toUpperCase()}
+              </Link>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 };
