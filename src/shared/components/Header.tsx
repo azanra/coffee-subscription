5a10105e509b36f15da654b22f36365ea36c2665
azan-personal <112457939+azanra@users.noreply.github.com>
2026-07-14 08:37:27 +0700

    feat: use mobile navbar on small screen

--- src/shared/components/Header.tsx
+++ src/shared/components/Header.tsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router";
 import CoffeeLogo from "../../assets/shared/desktop/logo.svg";
 import HamburgerLogo from "../../assets/shared/mobile/icon-hamburger.svg";
+import CloseLogo from "../../assets/shared/mobile/icon-close.svg";
 import useIsMobileScreen from "../hooks/useIsMobileScreen";
 
+import { useState } from "react";
+
 const Header = () => {
+  const [isShow, setIsShow] = useState(false);
   const { isMobileScreen } = useIsMobileScreen();
 
   const navigationTab = [
@@ -13,13 +17,31 @@ const Header = () => {
   ];
 
   return (
-    <div className="flex justify-between mb-[40px]">
-      <img src={CoffeeLogo} alt="coffee-logo" />
+    <div className="mb-[40px]">
+      <div className="flex justify-between items-center">
+        <img src={CoffeeLogo} alt="coffee-logo" />
 
-      {isMobileScreen ? (
-        <img src={HamburgerLogo} alt="hamburger-logo" />
-      ) : (
-        <div className="flex gap-[33px]">
+        {isMobileScreen ? (
+          <img
+            src={isShow ? CloseLogo : HamburgerLogo}
+            alt="hamburger-logo"
+            onClick={() => setIsShow(!isShow)}
+          />
+        ) : (
+          <div className="flex gap-[33px]">
+            {navigationTab.map((nav) => {
+              return (
+                <Link to={nav.to} key={nav.to}>
+                  {nav.text.toUpperCase()}
+                </Link>
+              );
+            })}
+          </div>
+        )}
+      </div>
+
+      {isShow && isMobileScreen && (
+        <div className="flex flex-col gap-[32px] text-center px-[82.5px] pt-[80px] pb-[54px]">
           {navigationTab.map((nav) => {
             return (
               <Link to={nav.to} key={nav.to}>
