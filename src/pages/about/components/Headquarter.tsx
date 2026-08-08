91ac0d2fb5922b68924a2e3fd705f8c03f4e92a2
azan-personal <112457939+azanra@users.noreply.github.com>
2026-08-08 12:04:28 +0700

    feat: add responsive headquarter

--- src/pages/about/components/Headquarter.tsx
+++ src/pages/about/components/Headquarter.tsx
@@ -1,11 +1,11 @@
-import Uk from "../../../assets/about/desktop/illustration-uk.svg";
-import Canada from "../../../assets/about/desktop/illustration-canada.svg";
-import Australia from "../../../assets/about/desktop/illustration-australia.svg";
+import Uk from "../../../assets/about/desktop/illustration-uk.svg?react";
+import Canada from "../../../assets/about/desktop/illustration-canada.svg?react";
+import Australia from "../../../assets/about/desktop/illustration-australia.svg?react";
 
 const Headquarter = () => {
   const headquarterList = [
     {
-      img: Uk,
+      svg: <Uk />,
       country: "United Kingdom",
       street: "68 Asfordby Rd",
       city: "Alcaston",
@@ -13,7 +13,7 @@ const Headquarter = () => {
       phoneNumber: "+44 1241 918425",
     },
     {
-      img: Canada,
+      svg: <Canada />,
       country: "Canada",
       street: "1528 Eglinton Avenue",
       city: "Toronto",
@@ -21,7 +21,7 @@ const Headquarter = () => {
       phoneNumber: "+1 416 485 2997",
     },
     {
-      img: Australia,
+      svg: <Australia />,
       country: "Australia",
       street: "36 Swanston Street",
       city: "Kewell",
@@ -30,19 +30,30 @@ const Headquarter = () => {
     },
   ];
   return (
-    <div>
-      <div>
-        <h1>Our headquarters</h1>
-      </div>
-      <div>
+    <div className="flex flex-col gap-[64px] xxl:px-[117.5px]">
+      <h1 className="text-preset-4 text-(--neutral-500) text-center md:text-start">
+        Our headquarters
+      </h1>
+
+      <div className="flex flex-col md:flex-row md:justify-between xxl:flex-[1_1_0] gap-[24px]">
         {headquarterList.map((item) => {
           return (
-            <div key={item.country}>
-              <img src={item.img} alt={item.country} />
-              <p>{item.street}</p>
-              <p>{item.city}</p>
-              <p>{item.postalCode}</p>
-              <p>{item.phoneNumber}</p>
+            <div
+              key={item.country}
+              className="flex flex-col gap-[32px] md:gap-[40px]"
+            >
+              <div className="flex items-center justify-center">{item.svg}</div>
+              <div className="text-center flex flex-col gap-[24px]">
+                <h2 className="text-preset-3 text-(--neutral-900)">
+                  {item.country}
+                </h2>
+                <div className="text-preset-6 text-(--neutral-900)">
+                  <p>{item.street}</p>
+                  <p>{item.city}</p>
+                  <p>{item.postalCode}</p>
+                  <p>{item.phoneNumber}</p>
+                </div>
+              </div>
             </div>
           );
         })}
