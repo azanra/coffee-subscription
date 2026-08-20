5128d6fe0f2aac34bc245303e9a58d7e347a42a7
azan-personal <112457939+azanra@users.noreply.github.com>
2026-08-20 08:14:25 +0700

    feat: add bg color to wrapper

--- src/shared/components/PageWrapper.tsx
+++ src/shared/components/PageWrapper.tsx
@@ -2,7 +2,7 @@ import type React from "react";
 
 const PageWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="px-[16px] py-[24px] md:px-[32px] md:py-[40px] xxl:px-[80px] xxl:py-[40px]">
+    <div className="px-[16px] py-[24px] md:px-[32px] md:py-[40px] xxl:px-[80px] xxl:py-[40px] bg-(--neutral-50)">
       {children}
     </div>
   );
